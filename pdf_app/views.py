134e35dd8cb1a69ec8214f7a08e675e1d3eb9b87
Mwalugha <mwalughabura@gmail.com>
2022-07-30 14:26:04 +0300

    pdf-app settings

--- pdf_app/views.py
+++ pdf_app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+# File contents: `app_pdf/views.py` (partial content)
+...
+from reportlab.pdfgen    import canvas
+from reportlab.lib.utils import ImageReader
+from datetime            import datetime
+...
+
+def pdf_dw(request):                                  
+
+    # Create the HttpResponse object 
+    response = HttpResponse(content_type='application/pdf') 
+
+    # This line force a download
+    response['Content-Disposition'] = 'attachment; filename="1.pdf"' 
+
+    # READ Optional GET param
+    get_param = request.GET.get('name', 'World')
+
+    # Generate unique timestamp
+    ts = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    p = canvas.Canvas(response)
+
+    # Write content on the PDF 
+    p.drawString(100, 500, "Hello " + get_param + " (Dynamic PDF) - " + ts ) 
+
+    # Close the PDF object. 
+    p.showPage() 
+    p.save() 
+
+    # Show the result to the user    
+    return response
